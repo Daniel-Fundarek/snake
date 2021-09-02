1bd23a9439830d4c27207b54bd4bbc2c14ec209d
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2021-09-02 20:35:01 +0200

    tailBlockDone

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -9,6 +9,7 @@ public class Main {
     // need to add hole or some kind of dangerous block
     // add resize feature (already prepared for that)
     // add main menu to select difficulty and speed maybe color of snake as well
+    // add random spawn point of snake (already prepared)
         // u can play the game with arrows. Dont hit an snake or wall otherwise u die. Eating Apples increase the length of the snake as well as increse score by one.
     new Controller();
     }
